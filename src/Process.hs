8a93e56f3197bc9a00a49b5e5c125c832610956c
StefanoBisi <stefano.bisi@aol.com>
2022-06-21 22:53:31 +0200

    Implemented the jump logic

--- src/Process.hs
+++ src/Process.hs
@@ -3,7 +3,7 @@ import Data.Word ( Word8 )
 import Control.Monad.State ( MonadState(state), State )
 import Utilities ( putInList, rollDown, rollUp )
 
-data ProgramState = Run | WaitIn | WaitOut | End | Debug deriving (Show)
+data ProgramState = Run | WaitIn | WaitOut | End | Debug deriving (Show, Eq)
 
 data OutputMode = SingleChar | Line | Buffer Int
 
@@ -57,11 +57,17 @@ nextCommand p
     where _ptr = instructionPointer p
           _max = length (code p) - 1
 
+previousCommand :: Process -> Process
+previousCommand p
+    | _ptr > 0 = p { instructionPointer = pred _ptr }
+    | otherwise = p { pState = End }
+    where _ptr = instructionPointer p
+
 setProgramState :: ProgramState -> State Execution ()
 setProgramState s = state $ \(vm, p) -> ((), (vm, p { pState = s}))
 
 alterMemory :: (Word8 -> Word8) -> State Execution ()
-alterMemory op = state $ \(vm, p) -> 
+alterMemory op = state $ \(vm, p) ->
     let _memory = memory vm
         _ptr = memoryPointer p
         _value = _memory !! _ptr
@@ -88,19 +94,50 @@ moveRight = moveMemory rollUp
 moveLeft :: State Execution ()
 moveLeft = moveMemory rollDown
 
--- instruction ,
+-- instruction , (input)
 insertValue :: Word8 -> State Execution ()
-insertValue value = state $ \(vm, p) -> 
+insertValue value = state $ \(vm, p) ->
     let _memory = memory vm
         _ptr = memoryPointer p
         upd_vm = vm { memory = putInList _memory _ptr value } in
     ((), (upd_vm, nextCommand p))
 
--- instruction .
+-- instruction . (output)
 getValue :: State Execution Char
 getValue = state $ \(vm, p) ->
     let value = chr . fromIntegral $ memory vm !! memoryPointer p in
     (value, (vm, nextCommand p))
+
+jumpToBracket :: (Process -> Process) -> Process -> Process
+jumpToBracket action p = nextStep p 1 where
+    nextStep p n =
+        let np = stepCount (getCommand p) n in
+        if pState p == End  || np == 0 then p
+        else nextStep (action p) np
+    stepCount c n
+        | c == '[' = n + 1
+        | c == ']' = n - 1
+        | otherwise = n
+
+jumpForward :: Process -> Process
+jumpForward = jumpToBracket nextCommand
+
+jumpBackward :: Process -> Process
+jumpBackward = jumpToBracket previousCommand
+
+-- instruction [
+openBlock :: State Execution ()
+openBlock = state $ \(vm, p) ->
+    if (memory vm !! memoryPointer p) == 0
+        then ((), (vm, jumpForward p))
+        else ((), (vm, nextCommand p))
+
+-- instruction ]
+closeBlock :: State Execution ()
+closeBlock = state $ \(vm, p) ->
+    if (memory vm !! memoryPointer p) == 0
+        then ((), (vm, nextCommand p))
+        else ((), (vm, jumpBackward p))
 
 -- comments
 proceed :: State Execution ()
